74b1a690c2abc2826fca8a35a3172624ec808103
Kobie-Kirven <51420086+Kobie-Kirven@users.noreply.github.com>
2021-03-16 13:57:46 -0400

    Fix imports

--- TTSIM.py
+++ TTSIM.py
@@ -3,18 +3,18 @@
 # simulations.
 #
 
-from tsimprep import Slicer
-from tsimprep import Structure
-from tsimprep import PrepPSF, Merge
+from ttsimprep import Slicer
+from ttsimprep import Structure
+from ttsimprep import PrepPSF, Merge
 
 seq = Slicer("fasta_test.fasta").slice(1, 60)
 
 Structure.buildStructure(seq,"out.pdb")
 
 vmd = "/Applications/VMD\ 1.9.4a48-Catalina-Rev7.app/Contents/vmd/vmd_MACOSXX86_64"
-# prep = PrepPSF(vmd,"out.pdb")
+prep = PrepPSF(vmd,"out.pdb")
 
-# prep.psf_builder()
+prep.psf_builder()
 
 comb = Merge(vmd, "out.psf","out.psf", "out_comb.psf")
 comb.mergePSF()
