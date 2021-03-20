ec46c42378a320996f752866228a5318c003133b
Kobie-Kirven <51420086+Kobie-Kirven@users.noreply.github.com>
2021-03-20 16:02:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TTSIM", # Replace with your own username
+    name="ttsim", # Replace with your own username
     version="0.0.1",
     author="Kobie Kirven",
     author_email="kjkirven@presby.edu",
     description="Transporter Termini SIMulation",
     long_description=long_description,
+    scripts=['./lib/ttsim.sh'],
     long_description_content_type="text/markdown",
     url="https://github.com/Kobie-Kirven/TTSIM",
-    project_urls={
-        "Bug Tracker": "https://github.com/Kobie-Kirven/TTSIM/issues",
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
